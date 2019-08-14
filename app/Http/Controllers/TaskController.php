2b9776f06e57b3cd330f10b4e703bf14a2990283
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-08-14 15:47:09 +0900

    Add index method
    モデルないのfolderメソッドからデータを引っ張ってきて
    返り値にテンプレートファイルを指定することでビューにデータを渡す

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return "Hello world";
+        // モデル内のFolderメソッドから配列でデータを引き出し変数に入れる処理
+        // データの入った変数はreturnで返り値を指定しviewに渡される
+        // viewの引数は第一引数に読み込むテンプレートファイル名、第二引数に実際に送るデータが書かれている
+        $folders = Folder::all();
+        return view ('tasks/index',[
+            'folders' => $folders,
+        ]);
     }
 }
