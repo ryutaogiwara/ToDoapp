84d196c5c470fa9737b21e5cfd31564bd5f2ee06
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-09-03 23:34:02 +0900

    Add task create
    タスク作成ビューを表示するためのメソッド追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -28,4 +28,26 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create',[
+            'folder_id' => $id
+        ]);
+    }
+
+    // // コントローラーメソッドが呼び出されるときに Laravel がリクエストの情報を Request クラスのインスタンス $request に詰めて引数として渡す
+    // public function create(CreateFolder $request)
+    // {
+    //     // フォルダモデルの新規インスタンスを作成
+    //     $folder = new Folder();
+    //     // $folderインスタンスのタイトルに$requestインスタンスのタイトル入力値を代入
+    //     $folder->title = $request->title;
+    //     // インスタンスの情報をDBに保存
+    //     $folder->save();
+
+    //     return redirect()->route('tasks.index', [
+    //         'id' => $folder->id,
+    //     ]);
+    // }
 }
