aa8cb1f494c4a8a9e898da28326c7d8cdace14d4
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-09-03 23:32:46 +0900

    Update web.php
    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.
 Route::post('/folders/create', 'FolderController@create');
 // タスク作成
 Route::get('folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('folders?{id}/tasks/create', 'TaskController@create');
+Route::post('folders/{id}/tasks/create', 'TaskController@create');
