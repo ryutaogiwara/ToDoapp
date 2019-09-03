4dfcd4fa4d3c24e5443c3777022bcc7c51ab9492
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-09-03 23:56:31 +0900

    Create layout faile
    部分テンプレート化

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ToDo App</title>
+  @yield('styles')
+  <link rel="stylesheet" href="/public/css/styles.css">
+</head>
+
+<body>
+  <header>
+    <nav class="my-navbar">
+      <a class="my-navbar-brand" href="/">ToDo App</a>
+    </nav>
+  </header>
+  <main>
+    @yield('content')
+  </main>
+  @yield('scripts')
+</body>
+
+</html>
