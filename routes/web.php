76973024b366de3a7604507e065373af9310e36b
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-09-05 18:22:43 +0900

    Add homepage index
    ホームページ表示のためのルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Folder;
-
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 // フォルダ作成
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+
+// タスク表示
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 // タスク作成
 Route::get('folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
 Route::post('folders/{id}/tasks/create', 'TaskController@create');
 // タスク編集
 Route::get('folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
 // ホーム画面
-Route::get('/', 'HomeController@index')->name('hhome');
+Route::get('/', 'HomeController@index')->name('home');
+
+// Authファサード
+Auth::routes();
