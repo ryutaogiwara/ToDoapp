2b0e3d931488ee4712dcbf90c0865827b9d91fa7
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-09-05 18:23:38 +0900

    Update ResetPasswordController.php
    微修正
    必要ないかも

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
