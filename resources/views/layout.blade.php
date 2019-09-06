7a5d157cb4a246f3bce8503fafffb74fd217dd2e
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-09-06 13:45:18 +0900

    header update
    ログイン濃霧でヘッダの振り分け処理実装
    ログアウト処理実装

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,12 +13,38 @@
   <header>
     <nav class="my-navbar">
       <a class="my-navbar-brand" href="/public/">ToDo App</a>
+      <div class="my-navbar-control">
+        <!-- ログインしてるかt/fで返す -->
+        @if(Auth::check())
+        <!-- true処理 -->
+        <span class="my-navbar-item">ようこそ, {{ Auth::user()->name }}さん</span>
+        |
+        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+        <form id="logut-form" action="{{ route('logout') }}" method="post" style="display: none;">
+          @csrf
+        </form>
+        @else
+        <!-- false処理 -->
+        <a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
+        |
+        <a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
+        @endif
+      </div>
     </nav>
   </header>
   <main>
     @yield('content')
   </main>
   @yield('scripts')
+  <!-- ログアウト処理 -->
+  @if(Auth::check())
+  <script>
+    document.getElementById('logout').addEventListener('click', function(event) {
+      event.priventDefault();
+      document.getElementById('logout-form').submit();
+    });
+  </script>
+  @endif
 </body>
 
 </html>
