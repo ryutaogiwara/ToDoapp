f87294271908e26bf5347101aebf448ddea23d8f
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-09-08 05:08:00 +0900

    Update web.php
    ルートグループ追加
    ネストさせることで
    まず、認証ミドルウェア（Auth）を適用。
    次に、認可ミドルウェア（フォルダポリシー）を必要に応じて適用している

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 // ミドルウェア経由で処理前に任意のプログラム実装
 // ここではAuthを参照してログインしていた場合のみ処理が行われる
-route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'auth'], function () {
   // ホーム画面
   Route::get('/', 'HomeController@index')->name('home');
 
@@ -9,18 +9,20 @@ route::group(['middleware' => 'auth'], function () {
   Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
   Route::post('/folders/create', 'FolderController@create');
 
-  // タスク作成
-  Route::get('folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-  Route::post('folders/{folder}/tasks/create', 'TaskController@create');
-  // タスク編集
-  Route::get('folders/{folder}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-  Route::post('folders/{folder}/tasks/{task_id}/edit', 'TaskController@edit');
+  Route::group(['middleware' => 'can:view,folder'], function () {
+    // タスク表示
+    Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+
+    // タスク作成
+    Route::get('folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+    Route::post('folders/{folder}/tasks/create', 'TaskController@create');
+
+    // タスク編集
+    Route::get('folders/{folder}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+    Route::post('folders/{folder}/tasks/{task_id}/edit', 'TaskController@edit');
+  });
 });
 
-route::group(['middleware' => 'can:view,folder'], function () {
-  // タスク表示
-  Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
-});
 // Authファサード
 // ログインしていない場合はログインページへリダイレクトされる
 Auth::routes();
